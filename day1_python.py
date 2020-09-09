07d1a42c97354dfa1fafdc06db03ef5779281418
Katy Donoghue <kathrynjdonoghue@gmail.com>
2020-09-09 13:12:28 -0400

    created functions for greeting and adding dollar amounts

---
+++ day1_python.py
@@ -0,0 +1,15 @@
+# def greeting(name, age):
+#     print(f"This is {name}, they are {age} years old")
+
+# greeting("Mary", 30)
+
+# my_name = input("Enter your name: ")
+# print(my_name)
+
+# #dollar amounts
+# num1 = input("Enter a dollar amount (ie $X.XX): ")
+# num2 = input("Enter a second dollar amount: ")
+
+# result = float(num1) + float(num2)
+
+# print(f"${result}")
