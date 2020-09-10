b7d20335687dc718145d390c6914bc3ca91ac1de
Katy Donoghue <kathrynjdonoghue@gmail.com>
2020-09-10 16:52:57 -0400

    palindrome assignment

---
+++ palindrome.py
@@ -0,0 +1,18 @@
+your_word = input("Enter a word: ")
+
+def is_palindrome(word):
+    return word == word[::-1]
+    if word == your_word:
+        is_palindrome(your_word) == True
+    else:
+        is_palindrome(your_word) == True
+ 
+if is_palindrome(your_word) == True:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
+    
+
+    
+
+       
