07d569fca5f3fbc454c92c09fa1203aada3b0625
Katy Donoghue <kathrynjdonoghue@gmail.com>
2020-09-11 09:44:49 -0400

    removed commented out code

--- palindrome.py
+++ palindrome.py
@@ -10,23 +10,4 @@ def is_palindrome(word):
 if is_palindrome(your_word) is True:
     print("Palindrome")
 else:
-    print("Not a Palindrome")
-
-# def is_palindrome(word):
-#     return word == word[::-1]
-#     if word == your_word:
-#         is_palindrome(your_word) == True
-#     else:
-#         is_palindrome(your_word) == False
- 
-# if is_palindrome(your_word) == True:
-#     print("Palindrome")
-# else:
-#     print("Not a Palindrome")
-
-
-
-
-    
-
-       
+    print("Not a Palindrome")     
